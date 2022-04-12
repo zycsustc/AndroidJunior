695b938b936dabfc7dda092c5ae7c57531f7b14e
yichi532 <yichi.zhang@thoughtworks.com>
2022-04-12 12:02:18 +0800

    [yichi][feat] add create contact function

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.myapplication
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.database.Cursor
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.view.View
-import android.view.Window
-import android.view.WindowManager
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+open class MainActivity : AppCompatActivity() {
+    private val ADD_CONTACT_REQUEST = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -19,8 +22,54 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
-    fun onPickContactClick(view: View) {
-        val intent = Intent(this, LoginActivity::class.java)
-        startActivity(intent)
+    fun onAddContactClick(view: View) {
+        val intent = Intent(ContactsContract.Intents.Insert.ACTION).apply {
+            // Sets the MIME type to match the Contacts Provider
+            type = ContactsContract.RawContacts.CONTENT_TYPE
+        }
+
+        intent.apply {
+            putExtra(ContactsContract.Intents.Insert.NAME, "Tim Allen")
+
+            putExtra(ContactsContract.Intents.Insert.EMAIL, "test@test.com")
+            putExtra(
+                ContactsContract.Intents.Insert.EMAIL_TYPE,
+                ContactsContract.CommonDataKinds.Email.TYPE_WORK
+            )
+            putExtra(ContactsContract.Intents.Insert.PHONE, "12345678")
+            putExtra(
+                ContactsContract.Intents.Insert.PHONE_TYPE,
+                ContactsContract.CommonDataKinds.Phone.TYPE_WORK
+            )
+        }
+
+        startActivityForResult(intent, ADD_CONTACT_REQUEST)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        print("get result")
+
+        if (requestCode == ADD_CONTACT_REQUEST && resultCode == RESULT_OK && data != null) {
+
+            print("get result data")
+
+            val cursor: Cursor = contentResolver.query(data.data!!,
+                arrayOf("display_name", "data1"), null, null, null)!!
+
+            cursor.moveToNext()
+
+            val contactName: String =
+                cursor.getString(cursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME))
+
+            val phoneNum: String =
+                cursor.getString(cursor.getColumnIndexOrThrow(ContactsContract.CommonDataKinds.Phone.NUMBER))
+
+            cursor.close()
+
+            Toast.makeText(this, "$contactName $phoneNum", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
